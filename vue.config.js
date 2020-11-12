0c32715b426185363bb794d826469ae7111f2698
Ovidiu Miclea <ovidiualx@gmail.com>
2020-11-12 18:19:00 +0200

    Add log system, add update system, add macos sign

--- vue.config.js
+++ vue.config.js
@@ -3,13 +3,32 @@ module.exports = {
     electronBuilder: {
       nodeIntegration: true,
       buildResources: "./build",
+      mac: {
+        "hardenedRuntime": true,
+        "gatekeeperAssess": false,
+        "entitlements": "build/entitlements.mac.plist",
+        "entitlementsInherit": "build/entitlements.mac.plist"
+      },
+      build: {
+        "afterSign": "scripts/notarize.js",
+        "publish": {
+          "provider": "github",
+          "owner": "zilliqa",
+          "repo": "ceres"
+        }
+      },
+      "dmg": {
+        "sign": false
+      },
       builderOptions: {
         appId: "com.ceres.app",
         productName: "Ceres",
         copyright: "Copyright © 2020 Zilliqa",
         extraResources: ["extra"],
         extraFiles: ["extra"],
+        "afterSign": "scripts/notarize.js"
       },
+      publish: ["github"]
     },
   },
   css: {
